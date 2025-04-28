5c79c3690da9ac35076c351b5c09371238fcfa77
arturodcs <a00833516@tec.mx>
2025-04-27 20:33:53 -0600

    test: add parser tests for various program structures

---
+++ tests/test_parser.py
@@ -0,0 +1,96 @@
+from src.parser.parser import parser
+from src.lexer.lexer import lexer
+
+def parse(code: str):
+    return parser.parse(code, lexer=lexer)
+
+def test_program_only_main():
+    code = """
+    program p0;
+
+    main {}
+
+    end
+    """
+    assert parse(code)
+
+
+def test_vars_and_arithmetic():
+    code = """
+    program arithmetic_demo;
+    var a, b, c: int;
+        x, y: float;
+
+    main {
+        a = 2 + 3 * (4 - 1);
+        x = 7.5 / 2.5 - -1.0;
+        print(a, x);
+    }
+    end
+    """
+    assert parse(code)
+
+
+def test_function_definition_and_call():
+    code = """
+    program with_funcs;
+
+    void sum(a: int, b: int) [
+        var result: int;
+        {
+            result = a + b;
+            print("Resultado:", result);
+        }
+    ];
+
+    main {
+        sum(4, 5);
+    }
+    end
+    """
+    assert parse(code)
+
+
+def test_condition_and_loop():
+    code = """
+    program control_flow;
+    var i, limit: int;
+
+    main {
+        i = 0;
+        limit = 3;
+
+        while (i < limit) do {
+            print("i =", i);
+            i = i + 1;
+        };
+
+        if (i != limit) {
+            print("Nunca debería verse");
+        };
+    }
+    end
+    """
+    assert parse(code)
+
+
+def test_print_strings_and_relops():
+    code = """
+    program printer;
+    var x: int;
+
+    main {
+        x = 10;
+        if (x > 0) {
+            print("Positivo");
+        } ;
+        if (x < 0) {
+            print("Negativo");
+        } else {
+            print("No es negativo");
+        };
+        if (x != 0) { print("No cero"); };
+    }
+    end
+    """
+    assert parse(code)
