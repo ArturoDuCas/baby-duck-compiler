5026df4cc99ab16f1a7c61d0f017c638163ce923
arturodcs <a00833516@tec.mx>
2025-05-13 15:48:24 -0600

    feat: implement operator hierarchy and precedence function

--- src/semantic/constants.py
+++ src/semantic/constants.py
@@ -4,3 +4,6 @@ GLOBAL_FUNC_NAME = "global"
 # type for the global function
 GLOBAL_FUNC_TYPE = "program"  
 VOID_FUNC_TYPE = "void"
+
+# used for the fake bottom during the quadruples generation
+FAKE_BOTTOM = "fake_bottom"
